848ba0e001dc64ee772fa02b9b03c86dc28f39ff
raunofreiberg <freiberggg@gmail.com>
2018-05-29 10:12:22 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,32 +2,20 @@ import React, {Timeout} from '../lib/react';
 import ReactDOM from '../lib/react-dom';
 import {createCache, createResource} from '../lib/simple-cache-provider';
 
-const data = {
-    '1337': 'Rauno',
-    '1231': 'John',
-    '99': 'Mary',
-};
-
-const fetchAsync = id => new Promise((resolve, reject) => {
-    if (data[id]) {
-        return setTimeout(
-            () => resolve(data[id]),
-            4000,
-        );
-    }
-
-    return setTimeout(
-        () => reject("Invalid ID"),
-        4000,
-    )
-});
+const fetcher = () =>
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.json());
 
 const cache = createCache();
-const resource = createResource(fetchAsync);
+const resource = createResource(fetcher);
 
-const AsyncName = ({ id }) => {
-    const name = resource.read(cache, id);
-    return <p>{name}</p>
+const Todos = () => {
+    const data = resource.read(cache);
+    return (
+        <div>
+            {data.slice(0,20).map(item => <p key={item.id}>{item.title}</p>)}
+        </div>
+    )
 };
 
 const Loading = () => ReactDOM.createPortal(
@@ -80,25 +68,21 @@ class Fallback extends React.Component {
     }
 }
 
-const AsyncComponent = () => (
-    <ErrorBoundary>
-        <Fallback timeout={2000} placeholder={<Loading />}>
-            <AsyncName id="1337" />
-            <p>Also, this text should appear with the async name.</p>
-        </Fallback>
-    </ErrorBoundary>
-);
-
 const App = () => (
     <React.Fragment>
-        <AsyncComponent />
+        <ErrorBoundary>
+            <Fallback timeout={1000} placeholder={<Loading />}>
+                <Todos />
+            </Fallback>
+        </ErrorBoundary>
         <h1>This is a demo of React Suspense</h1>
+        <h3>Switch to "Slow 3G" connection via the Network tab to simulate the experience of a slower connection.</h3>
         <ul>
-            <li>An async API request is simulated using setTimeout with a 4 second delay.</li>
-            <li>The placeholder is shown when the request takes <b>more than 2 seconds.</b></li>
+            <li>An actual API request is executed which may take time based on the connection speed.</li>
+            <li>The placeholder is shown when the request takes <b>more than one second.</b></li>
         </ul>
-        <p>Thus, on faster devices there is no need to clutter the UI with a immediate placeholder (i.e if the request were to resolve in a single second).</p>
-        <p>Whereas on slower devices, a placeholder is shown after the request takes way too long (2 seconds in this case).</p>
+        <p>On faster devices there is no need to clutter the UI with a immediate placeholder (i.e if the request were to resolve in less than a second).</p>
+        <p>Whereas on slower devices, a placeholder is shown after the request takes way too long (1 second in this case).</p>
     </React.Fragment>
 );
 
